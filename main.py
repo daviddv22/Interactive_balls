5790d2d4e651a37047cc72ec7052c5ba7b2c3de6
David Doan <daviddoan@Davids-MacBook-Pro-193.local>
2024-02-13 03:21:31 -0500

    refactor

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import pygame
-import random
+import random as rand
 import math
 import hyperparameter as hp
-
-
 
 # Initialize pygame variables
 pygame.init()
 window = pygame.display.set_mode((hp.WIDTH, hp.HEIGHT))
-selected_ball_index = None  # Keep track of the selected ball
+# player ball index (selected ball)
+selected_ball_index = None
 balls = []
 
 # Game loop setup
@@ -17,31 +16,39 @@ clock = pygame.time.Clock()
 
 # Ball Functions
 
-# add the ball to the screen with a click
+# called when user clicks on the screen
+# adds a ball to the screen at the position of the click
 def add_ball(position):
-    ball = {
+    newBall = {
         'position': list(position),
-        'velocity': [random.randint(-hp.MAX_VELOCITY, hp.MAX_VELOCITY), random.randint(-hp.MAX_VELOCITY, hp.MAX_VELOCITY)],
-        'color': (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        'velocity': [rand.randint(-hp.MAX_VELOCITY, hp.MAX_VELOCITY), rand.randint(-hp.MAX_VELOCITY, hp.MAX_VELOCITY)],
+        'color': (rand.randint(0, 255), rand.randint(0, 255), rand.randint(0, 255))
     }
-    balls.append(ball)
+    balls.append(newBall)
 
-# check if the click is on a ball
-def is_click_on_ball(click_pos):
+# checks if the mouse click is on a ball
+def is_click_on_ball(click_position):
     for ball in balls:
-        distance = math.sqrt((ball['position'][0] - click_pos[0]) ** 2 + (ball['position'][1] - click_pos[1]) ** 2)
-        if distance < hp.BALL_RADIUS:
+        ballDistanceFromClick = get_distance_click_ball(click_position, ball)
+        
+        if ballDistanceFromClick < hp.BALL_RADIUS:
             return True
     return False
 
 # move the selected ball in the inputted direction
 def move_selected_ball(dx, dy):
     if selected_ball_index is not None:
-        # prevent the ball from going off the screen
-        if balls[selected_ball_index]['position'][0] + dx < hp.BALL_RADIUS or balls[selected_ball_index]['position'][0] + dx > hp.WIDTH - hp.BALL_RADIUS:
+        # screen boundaries conditions
+        ballXPos = balls[selected_ball_index]['position'][0]
+        ballYPos = balls[selected_ball_index]['position'][1]
+        if ballXPos + dx < hp.BALL_RADIUS or \
+            ballXPos + dx > hp.WIDTH - hp.BALL_RADIUS:
             dx = 0
-        if balls[selected_ball_index]['position'][1] + dy < hp.BALL_RADIUS or balls[selected_ball_index]['position'][1] + dy > hp.HEIGHT - hp.BALL_RADIUS:
+        if ballYPos + dy < hp.BALL_RADIUS or \
+            ballYPos + dy > hp.HEIGHT - hp.BALL_RADIUS:
             dy = 0
+    
+        # move the ball
         balls[selected_ball_index]['position'][0] += dx
         balls[selected_ball_index]['position'][1] += dy
 
@@ -49,30 +56,32 @@ def move_selected_ball(dx, dy):
 def delete_selected_ball():
     if selected_ball_index is not None:
         del balls[selected_ball_index]
+        selected_ball_index = None
 
-# select the ball if the click is on a ball
+# get the index of the ball that was clicked on
 def select_ball(click_pos):
     global selected_ball_index
-    selected_ball_index = None
     for i, ball in enumerate(balls):
-        distance = math.sqrt((ball['position'][0] - click_pos[0]) ** 2 + (ball['position'][1] - click_pos[1]) ** 2)
+        distance = get_distance_click_ball(click_pos, ball)
         if distance < hp.BALL_RADIUS:
             selected_ball_index = i
-            # set velocity to 0
+
+            # stop the ball from moving so the user knows they have selected it
             balls[selected_ball_index]['velocity'] = [0, 0]
             break
 
-# Move the ball
-def move_ball(ball):
+# Model the movement of the ball
+def move_ball(ball):    
     ball['position'][0] += ball['velocity'][0]
     ball['position'][1] += ball['velocity'][1]
-    # Bounce off walls
+
+    # Check boundary conditions: If true, Bounce off walls
     if ball['position'][0] <= hp.BALL_RADIUS or ball['position'][0] >= hp.WIDTH - hp.BALL_RADIUS:
         ball['velocity'][0] *= -1
     if ball['position'][1] <= hp.BALL_RADIUS or ball['position'][1] >= hp.HEIGHT - hp.BALL_RADIUS:
         ball['velocity'][1] *= -1
 
-# Bounce balls off each other
+# Checks if two balls are overlapping and if so, bounces them off each other
 def bounce_balls(ball1, ball2):
     dx = ball1['position'][0] - ball2['position'][0]
     dy = ball1['position'][1] - ball2['position'][1]
@@ -83,66 +92,68 @@ def bounce_balls(ball1, ball2):
         # Calculate new velocities
         nx = dx / distance
         ny = dy / distance
-        p = 2 * (ball1['velocity'][0] * nx + ball1['velocity'][1] * ny - ball2['velocity'][0] * nx - ball2['velocity'][1] * ny) / 2
-        ball1['velocity'][0] -= p * nx
-        ball1['velocity'][1] -= p * ny
-        ball2['velocity'][0] += p * nx
-        ball2['velocity'][1] += p * ny
+        relativeVelocity = 2 * (ball1['velocity'][0] * nx + ball1['velocity'][1] * ny - ball2['velocity'][0] * nx - ball2['velocity'][1] * ny) / 2
 
-        overlap = 2 * BALL_RADIUS - distance
+        ball1['velocity'][0] -= relativeVelocity * nx
+        ball1['velocity'][1] -= relativeVelocity * ny
+        ball2['velocity'][0] += relativeVelocity * nx
+        ball2['velocity'][1] += relativeVelocity * ny
+
+        # Adjust the position of the balls so they don't overlap
+        overlap = 2 * hp.BALL_RADIUS - distance
         correction_factor = overlap / distance / 2  # Divide the overlap distance between the two balls
         ball1['position'][0] += correction_factor * dx
         ball1['position'][1] += correction_factor * dy
         ball2['position'][0] -= correction_factor * dx
         ball2['position'][1] -= correction_factor * dy
 
-# Draw the ball
+# Draw the ball onto the window
 def draw_ball(ball):
     pygame.draw.circle(window, ball['color'], (int(ball['position'][0]), int(ball['position'][1])), hp.BALL_RADIUS)
 
+# Get the distance between the click and the ball
+def get_distance_click_ball(click_pos, ball):
+    return math.sqrt((ball['position'][0] - click_pos[0]) ** 2 + (ball['position'][1] - click_pos[1]) ** 2)
+    
+
 def main():
     global running, selected_ball_index, balls
-    # Colors and ball settings
 
-    # Initialize balls
+    # Initialize balls given the initial settings
     for _ in range(hp.START_NUM_BALLS):
-        # set the ball's position, velocity, and color
-        ball = {
-            'position': [random.randint(hp.BALL_RADIUS, hp.WIDTH - hp.BALL_RADIUS), random.randint(hp.BALL_RADIUS, hp.HEIGHT - hp.BALL_RADIUS)],
-            'velocity': [random.randint(-hp.MAX_VELOCITY, hp.MAX_VELOCITY), random.randint(-hp.MAX_VELOCITY, hp.MAX_VELOCITY)],
-            'color': (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        }
-
-        # add the ball to the list of balls
-        balls.append(ball)
+        add_ball([rand.randint(hp.BALL_RADIUS, hp.WIDTH - hp.BALL_RADIUS), rand.randint(hp.BALL_RADIUS, hp.HEIGHT - hp.BALL_RADIUS)])
         
 
     while running:
+        window.fill(hp.BACKGROUND_COLOR)
         # input handling
         for event in pygame.event.get():
+            # if the user closes the window, exit the game
             if event.type == pygame.QUIT:
                 running = False
+            # if the user clicks on the screen, add a ball if the click is not on a ball
+            # otherwise, select the ball that was clicked on
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if not is_click_on_ball(event.pos):
                     add_ball(event.pos)
                 else:
                     select_ball(event.pos)
+            
+            # if the user presses the delete key, delete the selected ball
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DELETE:
                     delete_selected_ball()
-                    selected_ball_index = None
 
-        keys = pygame.key.get_pressed()  # Get the state of all keyboard buttons
+        # define the movement of the selected ball based on the user's input through the arrow keys
+        keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            move_selected_ball(-10, 0)
+            move_selected_ball(-hp.DEFAULT_MOVEMENT, 0)
         if keys[pygame.K_RIGHT]:
-            move_selected_ball(10, 0)
+            move_selected_ball(hp.DEFAULT_MOVEMENT, 0)
         if keys[pygame.K_UP]:
-            move_selected_ball(0, -10)
+            move_selected_ball(0, -hp.DEFAULT_MOVEMENT,)
         if keys[pygame.K_DOWN]:
-            move_selected_ball(0, 10)
-
-        window.fill(hp.BACKGROUND_COLOR)
+            move_selected_ball(0, hp.DEFAULT_MOVEMENT,)
 
         i = 0
         # interactive game loop
@@ -153,18 +164,21 @@ def main():
                 distance = math.sqrt(dx**2 + dy**2)
 
                 if distance < 2 * hp.BALL_RADIUS:
-                    del balls[i]  # Remove the touched ball
+                    # remove balls that the selected ball touches
+                    del balls[i]
+
                     # Adjust selected_ball_index if necessary
                     if i < selected_ball_index:
                         selected_ball_index -= 1
+
                     # set the selected ball velocity to 0
                     balls[selected_ball_index]['velocity'] = [0, 0]
-                    continue  # Skip the rest of the loop to avoid incrementing i
+                    continue 
                     
             move_ball(balls[i])
             i += 1
 
-        # Bounce balls off each other
+        # Loop through all the balls and bounce them off each other if applicable
         for j in range(len(balls)):
             for k in range(j + 1, len(balls)):
                 # if the ball is the selected ball, don't bounce it
@@ -172,17 +186,18 @@ def main():
                     continue
                 bounce_balls(balls[j], balls[k])
 
-        # Draw the balls
+        # Draw the balls onto the window
         for ball in balls:
             draw_ball(ball)
-
-        pygame.display.flip()
-        clock.tick(60)
 
         # if the selected ball is the only ball left, win game and exit
         if selected_ball_index is not None and len(balls) == 1:
             running = False
 
+        pygame.display.flip()
+        clock.tick(60)
+
+    # quits the game if the game is no longer "running"
     pygame.quit()
 
 if __name__ == "__main__":
